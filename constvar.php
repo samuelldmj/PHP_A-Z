8f45092d332c7adbff6eeb3993bdccd4a1763b21
Samuel <samuelldmj5@gmail.com>
2024-01-02 18:44:06 +0100

    first commit

---
+++ constvar.php
@@ -0,0 +1,16 @@
+<?php 
+
+//use define() to define a constant
+//it is defined at compiled time
+define('NAME', 'value');
+/* where "NAME" is the name of the contstant variable,
+"value"
+ */
+echo NAME . "<br>";
+//to check if a constant have been defined use defined()
+echo defined("NAME");
+
+//using the const keyword
+
+
+?>
