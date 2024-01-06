0e2a51eb5903495d0b5c8c80d0e47d0fe361c138
Samuel <samuelldmj5@gmail.com>
2024-01-06 05:09:09 +0100

    operator 1

---
+++ ops1.php
@@ -0,0 +1,43 @@
+<?php 
+//OPERATORS PART 1
+
+//Arithmetic operators: ( +, - , / , *, %, **)
+//when the division is without a remainder we obtain an integer
+//when the division is with a remainder we obtain a float
+//when an integer is divided by float we obtain a float
+$x = 10;
+$y = 2;
+var_dump($x - $y) . "<br>";
+var_dump($x + $y) . "<br>";
+var_dump($x / $y) . "<br>";
+var_dump($x % $y) . "<br>";
+var_dump($x ** $y) . "<br>";
+
+//plus(+) and minus(-) can be used to convert a value or variable to integer
+$x =  +"10";
+$y = 4;
+$z = "14";
+echo var_dump($x);
+echo var_dump(-$z);
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
