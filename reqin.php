69dba285a39bdea76bf0fee76436403de467897d
Samuel <samuelldmj5@gmail.com>
2024-01-18 20:51:24 +0100

    require and include learning

--- reqin.php
+++ reqin.php
@@ -14,11 +14,11 @@ but requires stops and throw an error */
 //require 
 // require "file.php"; 
 // require "file.php"; 
-//the require will continue to execute as long as it is called and the file is available, as seen above
+//the require will execute as long as it is called and the file is available, as seen above
 //output:Fatal error: Uncaught Error:...
 
 //require_once and include_once
-//this will include the file once if it hasn't be included already as seen below
+//this will include the file once, if it hasn't be included already as seen below
 // require_once "file.php";
 // $x++;
 // echo $x . "<br>";//output: 6
@@ -26,7 +26,7 @@ but requires stops and throw an error */
 // require_once "file.php";
 // echo $x . "<br>"; //output: 6
 
-//require overwrites the additional code written to complement the required files. see below
+//require overwrites the additional code written to complement the required files. see the two examples below
 // require "file.php";
 // $x++;
 // echo $x . "<br>";//output: 6
