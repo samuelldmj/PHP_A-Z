14f04a21ab833e181c3fd73efdf7952da22fbd60
Samuel <samuelldmj5@gmail.com>
2024-01-29 14:56:08 +0100

    clean array function file

--- helper.php
+++ helper.php
@@ -5,7 +5,8 @@ function prettyPrintarray(array $value){
     echo "</pre>";
 }
 
-
+//the function above is to make my array output pretty, when i call prettyPrintarray().
+//i will need to include it in any files that requires me to use array
 
 
 
